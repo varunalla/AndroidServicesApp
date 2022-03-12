47ef6df3b504a544054e6fbaabeb7c3259a9e20e
Varun Alla <varun.alla@sjsu.edu>
2022-03-12 00:13:33 -0800

    Added Service Implementation for downloading Files

--- app/src/main/java/com/example/androidservicesapp/MainActivity.java
+++ app/src/main/java/com/example/androidservicesapp/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.androidservicesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -16,9 +21,12 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     int requestCount=0;
+    boolean isRunning=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,6 +104,30 @@ public class MainActivity extends AppCompatActivity {
         return urls;
     }
     public void startDownload(View view){
+
+        Context context = getApplicationContext();
+
+        Button btn=(findViewById(R.id.button));
+        // use this to start and trigger a service
+        Intent i= new Intent(context, DownloaderService.class);
+        if(!isRunning){
+            // potentially add data to the intent
+            ArrayList<URL> urls=fetchUrls();
+            i.putExtra("urls",urls);
+            //i.putExtra("KEY1", "Value to be used by the service");
+            context.startService(i);
+            btn.setText(R.string.pd_act_btn_name_downloading);
+            //btn.setEnabled(false);
+            isRunning=true;
+        }
+        else{
+            context.stopService(i);
+            btn.setText(R.string.pd_act_btn_name);
+            isRunning=false;
+        }
+
+
+        /*
         ProgressBar bar=(findViewById(R.id.progressBar));
         bar.setProgress(0,true);
         Button btn=(findViewById(R.id.button));
@@ -106,98 +138,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<URL> urls=fetchUrls();
         this.requestCount=0;
         for(int i=0;i<urls.size();i++)
-            new Thread(new FileDownloader(urls.get(i),i+1)).start();
-    }
-    class FileDownloader implements Runnable{
-        int i;
-        public URL url;
-        public FileDownloader(URL url, int i){
-            this.url=url;
-            this.i=i;
-        }
-        public HttpURLConnection setConnectionProperties(HttpURLConnection urlConnection) throws Exception{
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setDoOutput(false);
-            urlConnection.addRequestProperty("Content-Type","application/octet-stream");
-            urlConnection.addRequestProperty("Accept","*/*");
-            urlConnection.addRequestProperty("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36");
-            urlConnection.addRequestProperty("Accept-Encoding","gzip, deflate, br");
-            urlConnection.addRequestProperty("Connection","keep-alive");
-            urlConnection.addRequestProperty("Host",this.url.getHost());
-            urlConnection.setUseCaches(false);
-            return urlConnection;
-        }
-        @Override
-        public void run() {
-
-                HttpURLConnection urlConnection = null;
-                try {
-                    urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection=this.setConnectionProperties(urlConnection);
-                    urlConnection.connect();
-                    File sdcard = getFilesDir();
-                    File new_file=new File(url.getFile());
-                    File file = new File(sdcard, new_file.getName());
-
-                    FileOutputStream fileOutput = new FileOutputStream(file);
-                    Log.i("responseCode",file.getName()+" : "+urlConnection.getResponseCode());
-                    InputStream inputStream =urlConnection.getResponseCode()==200? urlConnection.getInputStream():urlConnection.getErrorStream();
-                    if(urlConnection.getResponseCode()==200) {
-
-                        byte[] buffer = new byte[1024];
-                        int bufferLength;
-
-                        while ((bufferLength = inputStream.read(buffer)) > 0) {
-                            fileOutput.write(buffer, 0, bufferLength);
-                        }
-                        fileOutput.close();
-                        Log.i("status","Downloaded File "+file.getName());
-                        runOnUiThread(() -> {
-                            synchronized (FileDownloader.class){
-                                ProgressBar bar=(findViewById(R.id.progressBar));
-                                bar.setProgress(bar.getProgress()+20,true);
-                                EditText editText=( findViewById(R.id.status));
-                                String message=getString(R.string.sdownload,file.getName());
-                                editText.setText(message);
-                            }
-                        });
-                    }
-                    else{
-                        Log.i("status","Could not downloaded File "+file.getName());
-                        runOnUiThread(() -> {
-                            synchronized (FileDownloader.class){
-                                EditText editText=( findViewById(R.id.status));
-                                String message=getString(R.string.fdownload,file.getName());
-                                editText.setText(message);
-                            }
-                        });
-                    }
-
-                } catch (IOException e) {
-                    Log.i("status","Failed to Download File ");
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    if(urlConnection!=null)
-                        urlConnection.disconnect();
-                }
-                runOnUiThread(() -> {
-                    synchronized (FileDownloader.class){
-                        requestCount++;
-                        if(requestCount==5)
-                        {
-                            Button btn=(findViewById(R.id.button));
-                            btn.setText(R.string.pd_act_btn_name);
-                            btn.setEnabled(true);
-                            EditText editText=( findViewById(R.id.status));
-                            String message=getString(R.string.finaldownload);
-                            editText.setText(message);
-                        }
-                    }
-                });
-
-        }
+            new Thread(new FileDownloader(urls.get(i),i+1)).start();*/
     }
 
 }
